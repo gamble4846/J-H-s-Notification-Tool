3f940f37430a70a5e1c10d5f7196e5ee3f67f837
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2022-05-10 09:34:29 +0530

    Fixed CROS Problem

--- Angular - code/JAndHsNotificationTool/src/app/Services/LoginService/login.service.ts
+++ Angular - code/JAndHsNotificationTool/src/app/Services/LoginService/login.service.ts
@@ -6,8 +6,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  APILINK = "https://script.google.com/macros/s/AKfycbx9ebea_WxtN7li5kWus2XqGZpERSZMGf7u19tZe6U3OPmx9Pz4ZCBsrRgrSWg7BXKU/exec";
+  // APILINK = "https://script.google.com/macros/s/AKfycbx9ebea_WxtN7li5kWus2XqGZpERSZMGf7u19tZe6U3OPmx9Pz4ZCBsrRgrSWg7BXKU/exec";
+  APILINK = "https://script.google.com/macros/s/AKfycbx-bFn5rk-eTKdGuMs1vS-Kt6XOnm-UCkizmtpc6E0TLUWePSJg8VNwxZwDqszuQUpM/exec";
   constructor(private http: HttpClient) { }
+
+  getOptions(){
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    return options;
+  }
 
   LoginUser(password:any, email:any) {
     let body = {
@@ -15,7 +24,7 @@ export class LoginService {
       "Email": email,
       "Password": password
     }
-    return this.http.post(this.APILINK, body);
+    return this.http.post(this.APILINK, body, this.getOptions());
   }
 
 }
