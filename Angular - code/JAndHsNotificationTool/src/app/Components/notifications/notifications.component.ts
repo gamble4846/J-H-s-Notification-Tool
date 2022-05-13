959417ce649c87c0d25b0c802d7533c67c078f32
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2022-05-13 15:31:07 +0530

    Notification User Name added And Button With New Section

--- Angular - code/JAndHsNotificationTool/src/app/Components/notifications/notifications.component.ts
+++ Angular - code/JAndHsNotificationTool/src/app/Components/notifications/notifications.component.ts
@@ -4,6 +4,7 @@ import { CompaniesService } from 'src/app/Services/CompaniesService/companies.se
 import { NotificationSERService } from 'src/app/Services/NotificationSERService/notification-ser.service';
 import { NotificationService } from 'src/app/Services/NotificationService/notification.service';
 import { SessionManagementService } from 'src/app/Services/SessionManagement/session-management.service';
+import { UserService } from 'src/app/Services/UserService/user.service';
 
 @Component({
   selector: 'app-notifications',
@@ -14,7 +15,9 @@ export class NotificationsComponent implements OnInit {
   fullPageLoading = false;
   notificationData:any = [];
   CompaniesData:any = [];
-  constructor(private Companies: CompaniesService, private SessionManagement:SessionManagementService, private Notification:NotificationService, private fb: FormBuilder, private NotificationSER: NotificationSERService) { }
+  UserData:any = [];
+  showTable=true;
+  constructor(private User: UserService, private Companies: CompaniesService, private SessionManagement:SessionManagementService, private Notification:NotificationService, private fb: FormBuilder, private NotificationSER: NotificationSERService) { }
 
   ngOnInit(): void {
     this.SessionManagement.redirectAccLogin('Notifications');
@@ -44,7 +47,25 @@ export class NotificationsComponent implements OnInit {
           else{
             this.CompaniesData = response.data;
             this.CompaniesData.pop();
-            this.AllDataRecieved();
+            this.User.GetUsers()
+            .subscribe((response:any) => {
+              response = JSON.parse(response);
+              if(response.status != "200"){
+                this.fullPageLoading = false;
+                this.Notification.HandleServerError(response.message);
+              }
+              else{
+                this.UserData = response.data;
+                this.UserData.pop();
+                console.log(this.UserData);
+                this.AllDataRecieved();
+                this.fullPageLoading = false;
+              }
+            },
+            (error) => {
+              this.fullPageLoading = false;
+              this.Notification.HandleServerError(error.message);
+            });
             this.fullPageLoading = false;
           }
         },
@@ -52,7 +73,6 @@ export class NotificationsComponent implements OnInit {
           this.fullPageLoading = false;
           this.Notification.HandleServerError(error.message);
         });
-        console.log(this.notificationData);
       }
     },
     (error) => {
@@ -64,8 +84,17 @@ export class NotificationsComponent implements OnInit {
   AllDataRecieved(){
     this.notificationData.forEach((noti:any) => {
       noti.CompanyName = this.CompaniesData.find((x:any) => x.CompanyID = noti.CompanyID).CompanyName;
+      noti.UserName = this.UserData.find((x:any) => x.UsersId = noti.UsersId).Name;
     });
 
     console.log(this.notificationData);
+  }
+
+  ShowNotificationForm(){
+    this.showTable=false;
+  }
+
+  ShowTable(){
+    this.showTable=true;
   }
 }
